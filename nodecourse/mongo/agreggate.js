6f05c23183a9a67cf2aeafe69b791d48680131be
cesch2805 <1060066@alumno.um.edu.mx>
2022-05-12 20:32:34 -0500

    añadir mongo

---
+++ nodecourse/mongo/agreggate.js
@@ -0,0 +1,33 @@
+var mongoClient = require('mongodb').MongoClient;
+
+var collectionName = 'prices';
+
+var databaseName = 'learnyoumongo';
+
+var size = process.argv[2];
+
+var url = 'mongodb://localhost:27017/learnyoumongo';
+
+mongoClient.connect(url, function(err, DB) {
+	var dataBase = DB.db(databaseName);
+
+	dataBase.collection(collectionName)
+			.aggregate([
+				{ $match: { size: size } },
+				{ $group: {
+					_id: 'average',
+					total: { $avg: '$price' } 
+				} }
+			]).toArray(function(err, results) {
+				if (err) {
+					console.log(err);
+				}
+				if (!results.length) {
+					throw new Error('No results found')
+				}
+				console.log(Number(results[0].total).toFixed(2));
+			});
+
+	DB.close();
+
+});
